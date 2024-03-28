6c5c6f9e175d2c4bc7d1c790830dfffbed0b64a5
MTariq <tariqwc99@gmail.com>
2024-03-28 13:52:19 +0500

    basic setup of rabbitMQ  publisher and consumer

---
+++ consumer/receive.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	// Setting up is the same as the publisher; we open a connection and a channel, and declare the queue from which we're going to consume. Note this matches up with the queue that send publishes to.
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to create a channel")
+	defer ch.Close()
+	// Note that we declare the queue here, as well. Because we might start the consumer before the publisher, we want to make sure the queue exists before we try to consume messages from it.
+	q, err := ch.QueueDeclare(
+		"hello", //queue name
+		false,   //durable
+		false,   //delete when unused
+		false,   //exclusive
+		false,   //no-wait
+		nil,     //arguments
+
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	// We're about to tell the server to deliver us the messages from the queue. Since it will push us messages asynchronously, we will read the messages from a channel (returned by amqp::Consume) in a goroutine.
+
+	msgs, err := ch.Consume(
+		q.Name, //queue from which we will consume messages
+		"",     //consumer
+		true,   //auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "failed to register a consumer")
+
+	var forever chan struct{}
+
+	go func() {
+		for msg := range msgs {
+			log.Printf("Received a message: %s", msg.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+
+}
